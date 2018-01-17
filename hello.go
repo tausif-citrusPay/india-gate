c43c469705c8d443c99f04f9a1446291a63118e0
vikram.choudhary <vikram.choudhary@payu.in>
2018-01-17 22:15:38 +0530

    added another lambda func

---
+++ hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// HelloHandler is handler func
+func HelloHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	response := "Hi " + request.Body
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       response,
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(HelloHandler)
+}
